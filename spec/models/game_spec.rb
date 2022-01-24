c4f74f39454804a36e3b695bbf1f46952d284d91
maks <mkozaev@mail.ru>
2022-01-24 20:27:52 +0300

    refactor tests

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -118,20 +118,21 @@ RSpec.describe Game, type: :model do
     end
   end
 
-  context '.current_game_question' do
-    it 'correct .current question' do
+  describe '.current_game_question' do
+    it 'return current question' do
       game_w_questions.current_level = 10
       expect(game_w_questions.current_game_question).to eq(game_w_questions.game_questions[10])
     end
   end
 
-  context '.previous_level' do
-    let(:new_level) { 5 }
+  describe '.previous_level' do
+    context 'when new level is 5' do
+      let(:new_level) { 5 }
 
-    it 'correct .previous level' do
-      # Ставим текущий уровень на один выше
-      game_w_questions.current_level = new_level + 1
-      expect(game_w_questions.previous_level).to eq(new_level)
+      it 'return previous level of game' do
+        game_w_questions.current_level = new_level + 1
+        expect(game_w_questions.previous_level).to eq(new_level)
+      end
     end
   end
 end
