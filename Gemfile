32a7c58d37fefa90adfe2f9f426775cd74904829
maks <mkozaev@mail.ru>
2022-01-29 19:20:03 +0300

    admin_user fixes

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'devise-i18n'
 gem 'rails-i18n', '~> 6.0.0'
 gem 'font-awesome-rails'
 gem 'rails-controller-testing'
+gem 'rails_admin'
 
 group :development, :test do
   gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
   devise_for :users
   resources :users, only: %i[index show]
